dcde8d39b861ab19b37fc214dda16cbe66e00cb5
dhdhsadj <maguinhomast2005@gmail.com>
2025-05-07 18:39:20 +0100

    Communication between app and website

--- app_empresas (1).sql
+++ app_empresas (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Abr-2025 às 14:22
+-- Tempo de geração: 04-Maio-2025 às 12:36
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.0.30
 
@@ -56,7 +56,11 @@ CREATE TABLE `empresas` (
 
 INSERT INTO `empresas` (`id`, `nome`, `email`, `senha`, `data_cadastro`) VALUES
 (15, 'Jorge', 'Jorge@gmail.com', '$2y$10$s7Ve0HZcXHP42y0g58ofhefH1KSC0oIc0ZCgtg9DET9mmtDrJFYQm', '2025-04-23 19:25:37'),
-(16, 'Diogo ', 'dm@gmail.com', '$2y$10$A9TdafBnFTnpgcfs/SnyCOxB5ILd0PMJk3iEuDyDJ3B2ocqRWdhay', '2025-04-23 19:26:03');
+(16, 'Diogo ', 'dm@gmail.com', '$2y$10$A9TdafBnFTnpgcfs/SnyCOxB5ILd0PMJk3iEuDyDJ3B2ocqRWdhay', '2025-04-23 19:26:03'),
+(17, 'Dd@gmail.com', 'dd@gmail.com', '$2y$10$VMSo2m2IUL0TfW4kY2lcPeH9Wi11B4oDZmvOXqiOk8uHxSjwmfiSa', '2025-04-26 18:37:35'),
+(20, 'Douglas sola', 'dg@gmail.com', '$2y$10$iRhDS1kL8V4Sxuxbatg5dOdnI7V5SJJOWBol4TMPwzVT9Kf5i3Ii6', '2025-04-30 18:58:21'),
+(21, 'FEr', 'fer@gmail.com', '$2y$10$gVkC1tSsNFcgkuHgWA8Y0esHFKcuNWbljVEAyWjzSWl/UdfKVSERy', '2025-05-03 10:59:35'),
+(22, 'FFG', 'F@gmail.com', '$2y$10$cH7y6KWv3W8IqTehgOjomurU7Usv542D1Pp5esC74sILKwWPRP/1O', '2025-05-04 10:05:07');
 
 --
 -- Índices para tabelas despejadas
@@ -83,7 +87,7 @@ ALTER TABLE `empresas`
 -- AUTO_INCREMENT de tabela `empresas`
 --
 ALTER TABLE `empresas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
